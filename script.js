d8405b06ddc4d114701a6251eac89978cf93e3a1
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-25 23:41:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+ // Get the browser name and version from the navigator object
+        const browserName = navigator.appName;
+        const browserVersion = navigator.appVersion;
+
+        // Get the element with the id "browser-info" and update its content
+        const browserInfoElement = document.getElementById("browser-info");
+        browserInfoElement.textContent = "You are using " + browserName + " version " + browserVersion;
