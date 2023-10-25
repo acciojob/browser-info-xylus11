254c6c4e7433a137cb30bc385130f478c27d2a4f
mangeshahireakaXylus11 <97844580+xylus11@users.noreply.github.com>
2023-10-25 23:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 //your JS code here. If required.
-//your JS code here. If required.
- // Get the browser name and version from the navigator object
-        const browserName = navigator.appName;
-        const browserVersion = navigator.appVersion;
-
-        // Get the element with the id "browser-info" and update its content
-        const browserInfoElement = document.getElementById("browser-info");
-        browserInfoElement.textContent = "You are using " + browserName + " version " + browserVersion;
+let a=document.getElementById("browser-info");
+let b=document.createElement("p")
+b.innerText="You are using "+  navigator.appName+" version "+   navigator.appVersion
+a.append(b);
